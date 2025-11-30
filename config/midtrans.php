987f0110455a0a40fa041c824e5d5d2e5495b611
felixwlimz <felixlim140@gmail.com>
2025-11-30 12:01:18 +0700

    Add midtrans prod

--- config/midtrans.php
+++ config/midtrans.php
@@ -3,7 +3,7 @@
 return [
     'server_key' => env('MIDTRANS_SERVER_KEY', 'your-server-key-here'),
     'client_key' => env('MIDTRANS_CLIENT_KEY', 'your-client-key-here'),
-    'is_production' => false,
+    'is_production' => true,
     'is_sanitized' => true,
     'is_3ds' => true,
 ];
